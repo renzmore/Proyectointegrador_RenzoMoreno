fb4c210488604dc3a62f3bf4337c3e267688e600
renzmore <rjmr33@gmail.com>
2023-03-11 14:42:40 -0300

    creación del backend

---
+++ Proyectointegrador_RenzoMoreno/BackEnd/renzmore/src/main/java/com/portfolio/renzmore/RenzmoreApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.renzmore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RenzmoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RenzmoreApplication.class, args);
+	}
+
+}
---
+++ Proyectointegrador_RenzoMoreno/BackEnd/renzmore/src/test/java/com/portfolio/renzmore/RenzmoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.renzmore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RenzmoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
